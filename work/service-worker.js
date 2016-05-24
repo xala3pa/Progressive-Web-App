2bf0db7f2b7ade1be4fb61c00387f2687558c61c
xala3pa <alvaro.salazar@ticketbis.com>
2016-05-24 23:23:10 +0200

    add list of files to cache

--- work/service-worker.js
+++ work/service-worker.js
@@ -1,5 +1,23 @@
 var cacheName = 'weatherPWA';
-var filesToCache = [];
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
