00ab7caaa4fbff739bc32bb8775eed6a681cffe3
xala3pa <alvaro.salazar@ticketbis.com>
2016-05-24 23:04:03 +0200

    Register the service worker if it is available

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -236,4 +236,10 @@
     app.saveSelectedCities();
   }
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+
 })();
