8ab37cf0f22ad7dd228f733614987941c38109ad
xala3pa <alvaro.salazar@ticketbis.com>
2016-05-24 23:22:18 +0200

    clean unused cache objects

--- work/service-worker.js
+++ work/service-worker.js
@@ -10,3 +10,17 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
